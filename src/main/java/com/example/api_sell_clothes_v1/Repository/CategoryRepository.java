df95850435e7f0da88cba39a9146faddef67f9d6
son261103 <sonphaman5@gmail.com>
2025-02-20 14:28:25 +0700

    update category pageable

--- src/main/java/com/example/api_sell_clothes_v1/Repository/CategoryRepository.java
+++ src/main/java/com/example/api_sell_clothes_v1/Repository/CategoryRepository.java
@@ -1,6 +1,8 @@
 package com.example.api_sell_clothes_v1.Repository;
 
 import com.example.api_sell_clothes_v1.Entity.Categories;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -30,4 +32,27 @@ public interface CategoryRepository extends JpaRepository<Categories, Long> {
     List<Categories> findAllByParentIdAndStatusIsTrue(Long parentId);
 
     List<Categories> findAllByParentIdIsNullAndStatusIsTrue();
+
+    Page<Categories> findByParentIdIsNullAndStatusAndNameContainingIgnoreCase(
+            Boolean status, String search, Pageable pageable);
+
+    Page<Categories> findByParentIdIsNullAndNameContainingIgnoreCase(
+            String search, Pageable pageable);
+
+    Page<Categories> findByParentIdIsNullAndStatus(
+            Boolean status, Pageable pageable);
+
+    Page<Categories> findByParentIdIsNull(Pageable pageable);
+
+    // For sub categories
+    Page<Categories> findByParentIdAndStatusAndNameContainingIgnoreCase(
+            Long parentId, Boolean status, String search, Pageable pageable);
+
+    Page<Categories> findByParentIdAndNameContainingIgnoreCase(
+            Long parentId, String search, Pageable pageable);
+
+    Page<Categories> findByParentIdAndStatus(
+            Long parentId, Boolean status, Pageable pageable);
+
+    Page<Categories> findByParentId(Long parentId, Pageable pageable);
 }
